8ab486f9e5c4f9daea544819f92997fe031dee71
ArthurAtamuradov <atamuradow.arthur@gmail.com>
2021-10-08 17:08:39 +0500

    playing with Graphql

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,5 +1,38 @@
+const users = [
+  {
+    name: "Mekan",
+    age: 30,
+    email: "mekan@mail.ru",
+  },
+  {
+    name: "Maksat",
+    age: 23,
+    email: "maksat@gmail.com",
+  },
+];
+
 module.exports = {
   test() {
-    return "Hello Graphql!!";
+    return {
+      count: Math.ceil(Math.random() * 10),
+      users,
+    };
+  },
+  random({ min, max, count }) {
+    const arr = [];
+    for (let i = 0; i < count; i++) {
+      const number = Math.random() * (max - min) + min;
+      arr.push(number);
+    }
+    return arr;
+  },
+  createTestUser({ user: { name, email } }) {
+    const user = {
+      name,
+      email,
+      age: Math.ceil(Math.random() * 30),
+    };
+    users.push(user);
+    return user;
   },
 };
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,6 +1,28 @@
 const { buildSchema } = require("graphql");
 
 module.exports = buildSchema(`
+type User {
+    name:String!
+    age:Int!
+    email:String!
+}
+
+type TestType{
+    count :Int!
+    users:[User!]!
+}
+
 type Query{
-    test:String!
-}`);
+    test:TestType!
+    random(min:Int!,max:Int!,count:Int!):[Float!]!
+}
+
+input UserInput {
+    name: String!
+    email:String!
+}
+
+type Mutation{
+    createTestUser(user:UserInput!):User!
+}
+`);
